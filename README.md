6446b817c8be2f5495a03aba1b19d40fb96685ac
Jaehyeong Lee <lawyer1996@hotmail.com>
2019-04-06 19:56:15 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,41 +1,44 @@
-# Jaehyeong's Electronic Shop
-
-![pic1](https://user-images.githubusercontent.com/32945767/55677836-10b9be80-58a4-11e9-8ae9-a9844e2971af.jpg)
-
-As a frequent user of online exchange and shopping websites such as OfferUp, Craiglist, and Amazon, I decided to make my own 
-*almost* fully functional E-Commercie personal website in HTML5, CSS3, React.js. Also used PayPal's REST APIs
-
-I've implemented an user interface where users can view the products, their descriptions, price, models, and then add them to their shopping carts.
-
-![pic2](https://user-images.githubusercontent.com/32945767/55677837-10b9be80-58a4-11e9-8762-37049d2a481e.jpg)
-
-
-![pic3](https://user-images.githubusercontent.com/32945767/55677838-10b9be80-58a4-11e9-8b80-663e827cab84.jpg)
-
-*There is an error to be fixed in cart.js*
-
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-# HOW TO START
 
 ## Available Scripts
 
-In order to start the project, run the following command in the directory where
-cloned project resides
+In the project directory, you can run:
 
 ### `npm start`
 
-Then access port 3000 by opening 'http://localhost:3000' to view the display
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will dynamically load upon any changes
-Use React devtools to see any listed error
-
-If you want to view the product in test mode, run:
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
 
 ### `npm test`
 
-Then
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run build`
 
-to build the app for production
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ src/data.js
@@ -0,0 +1,140 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Apple iPhone 8 (64GB) - Silver [Locked to Simple Mobile Prepaid]",
+    img: "img/product-1.png",
+    price: 649.00,
+    company: "Apple",
+    info:
+        "This phone is locked to Simple Mobile from Tracfone, which means this device can only be used on the Simple Mobile wireless network.\n" +
+        "Simple Mobile offers a variety of coverage plans, including 30-Day Unlimited Talk, Text & Data. No activation fees, no credit checks, & no hassles on a nationwide lightning-fast network. For more information or plan options, please visit the Simple Mobile website.\n" +
+        "You’ll receive a Simple Mobile SIM kit with this iPhone. Follow the instructions to get service activated with the Simple Mobile plan of your choice.\n" +
+        "44. 7-Inch Retina HD display with true Tone\n" +
+        "IP67 water and dust resistant (maximum depth of 1 meter up to 30 minutes)\n" +
+        "12MP camera with OIS and 4K video\n" +
+        "7MP FaceTime HD Camera with Retina flash",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung Galaxy S7 32GB Factory Unlocked GSM LTE Smartphone (Black)",
+    img: "img/product-2.png",
+    price: 329.98,
+    company: "SAMSUNG",
+    info:
+        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "iPhone 6s 32GB Rose Gold",
+    img: "img/product-3.png",
+    price: 259.99,
+    company: "Apple",
+    info:
+        "This device is in EXCELLENT cosmetic condition, it is 100% fully functional as tested by our expert technicians and passed an industry leading 65-point inspection. The device may have minor cosmetic wear such as minimal light scratches on the screen and minor imperfections on sides and/or back. The device has been thoroughly inspected, cleaned and refurbished (if necessary) at our manufacturer approved repair center. Includes New OEM A/C Adapter & Data Cable. Devices do not include a SIM card, headphones, original packaging or the original manufacturer’s warranty. This CDMA UNLOCKED device is being sold for use with CDMA carrier networks such as Verizon and Sprint.  Please check with your carrier for compatibility.\n" +
+        "\n",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Apple iPhone XR (64GB) - (PRODUCT)RED [works exclusively with Simple Mobile]",
+    img: "img/product-4.png",
+    price: 749.99,
+    company: "Apple",
+    info:
+        "6.1-inch Liquid Retina display (LCD)\n" +
+        "IP67 water and dust resistant (max depth of 1 m up to 30 mins)\n" +
+        "12MP camera with OIS and 7MP TrueDepth front camera\n" +
+        "Face ID for secure authentication and Apple Pay\n" +
+        "A12 Bionic with next-generation Neural Engine\n" +
+        "Wireless charging—works with Qi chargers\n" +
+        "No Contract Wireless, No Activation Fees, No Credit Checks, & No Hassles On a Nationwide Lightning-fast Network.\n" +
+        "To activate your device, please visit the SIMPLE Mobile website and Activate your new phone with a SIMPLE Mobile 30-day service plan\n" +
+        "Note: This phone is carrier locked; Customers must have had their locked device activated on SIMPLE Mobile service for no fewer than 12 months, redeemed air time cards in no fewer than 12 months, and not have had their telephone number recycled or ported.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Apple iPhone X (64GB) - Silver [Locked to Simple Mobile Prepaid]",
+    img: "img/product-5.png",
+    price: 899.00,
+    company: "Apple",
+    info:"5. 8-inch Super Retina display (OLED) with HDR ¹\n" +
+        "IP67 water and dust resistant (max depth of 1m up to 30 mines) ³\n" +
+        "12MP dual cameras with dual OIS and 7MP True Depth front camera—Portrait mode and Portrait Lighting\n" +
+        "Note: Phone is locked to Simple Mobile and is not fully unlockable\n" +
+        "Face ID for secure authentication and Apple Pay ⁴. A11 Bionic with Neural Engine\n" +
+        "Wireless charging—works with Qi chargers ²\n" +
+        "iOS 12 with Emoji, Screen Time, Siri Shortcuts, and Group FaceTime\n" +
+        "Note: This iPhone is carrier locked; customers must have had their locked device activated on Simple Mobile service for no fewer than 12 months, redeemed air time cards in no fewer than 12 months, and not have had their recycled or ported.\n" +
+        "No Contract Wireless, No Activation Fees, No Credit Checks, & No Hassles On a Nationwide Lightning-fast Network.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Google Nexus 7 4G LTE Tablet by ASUS, Black 7-Inch 32GB (Verizon Wireless)",
+    img: "img/product-6.png",
+    price: 239.99,
+    company: "Google",
+    info:
+        "With the Nexus 7-G2, experience a clear and vibrant 7\" display plus easy access to your favorite apps and games — all in the palm of your hand.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Apple iPhone 1st Generation - 4GB - Black (AT&T) A1203 (GSM)",
+    img: "img/product-7.png",
+    price: 3000.00,
+    company: "Apple",
+    info:
+        "Apple iPhone 1st Generation runs on iOS operating system and has 1400 mAh battery capacity. This smartphone features a convenient 2.0MP camera, so you can capture images and store them on 4 GB memory space. This Apple smartphone supports GSM 850/900/1800/1900 (Quadband) and 2G networks. It comes in black. This device is locked to AT&T and compatible with Virgin Mobile, TracFone, Xfinity, Sprint, Verizon, U.S. Cellular, metroPCS, T-Mobile, Straight Talk, Cricket, Boost Mobile Carriers.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Samsung 16GB Galaxy S5 - No Contract (Unlocked), Certified Refurbished, Black",
+    img: "img/product-8.png",
+    price: 116,
+    company: "Apple",
+    info:
+        "The product ships with all relevant accessories but does not include a headphone or SIM card, and may arrive in a generic box.\n" +
+        "This Certified Refurbished product is backed by a minimum 90-day warranty. Only select sellers who maintain a high performance bar may offer Certified Refurbished products on Amazon.\n" +
+        "Display: 5.1-inches, Camera: 16-MP, Processor Speed: 2.5 GHz, OS: Android 4.4.2 (KitKat)",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
